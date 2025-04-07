a80df7bc1e39d786ab61ba5ba49683f2fa46ee81
Scott Lloyd <scott16lloyd@gmail.com>
2025-04-07 15:45:00 +0100

    Added additonal comments to improve explanation

--- LongestCommonSubsequence.java
+++ LongestCommonSubsequence.java
@@ -11,8 +11,10 @@ public class LongestCommonSubsequence {
     }
 
     public String compare() {
-        int[][] c = new int[X.length() + 1][Y.length() + 1];
-        String[][] s = new String[X.length() + 1][Y.length() + 1];
+        // Create a table the current LCS and the final LCS
+        // + 1 used to accomodate for the base case 0
+        int[][] c = new int[X.length() + 1][Y.length() + 1]; // Store length of current LCS
+        String[][] s = new String[X.length() + 1][Y.length() + 1]; // Store final LCS
 
         // Initialise first row and column
         for (int i = 0; i <= X.length(); i++) {
@@ -30,21 +32,21 @@ public class LongestCommonSubsequence {
             for (int j = 1; j <= Y.length(); j++) {
                 // If two characters are equal increase the LCS length
                 if (X.charAt(i - 1) == Y.charAt(j - 1)) {
-                    c[i][j] = c[i - 1][j - 1] + 1;
-                    s[i][j] = s[i - 1][j - 1] + X.charAt(i - 1);
+                    c[i][j] = c[i - 1][j - 1] + 1; // Add 1 to the diagonal value
+                    s[i][j] = s[i - 1][j - 1] + X.charAt(i - 1); // Add the matched character to the LCS
                 } else {
                     // Otherwise take the maximum of two previous cases
                     if (c[i - 1][j] >= c[i][j - 1]) {
-                        c[i][j] = c[i - 1][j];
-                        s[i][j] = s[i - 1][j];
+                        c[i][j] = c[i - 1][j]; // Value from cell above
+                        s[i][j] = s[i - 1][j]; // LCS from cell above
                     } else {
-                        c[i][j] = c[i][j - 1];
-                        s[i][j] = s[i][j - 1];
+                        c[i][j] = c[i][j - 1]; // Value from cell on the left
+                        s[i][j] = s[i][j - 1]; // LCS from cell on the left
                     }
                 }
             }
         }
-        return s[X.length()][Y.length()];
+        return s[X.length()][Y.length()]; // Return LCS
     }
 
     public static void main(String[] args) {
